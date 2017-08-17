5c4bddc5ea22ee92fa7d0984a0a2f36aaeb8e210
Yan Zhang <yan.zhang@insureon.com>
2017-08-17 18:44:11 -0500

    add data access layer and run success

---
+++ ProjectManager/DAL/GanttContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using ProjectManager.Models;
+
+namespace ProjectManager.DAL
+{
+    public class GanttContext : DbContext
+    {
+        public GanttContext() : base("GanttContext") { }
+
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Link> Links { get; set; }
+    }
+}
