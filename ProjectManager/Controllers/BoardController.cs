0ead8c24e26ef7e64ed52ec8adf27b305765efe8
Yan Zhang <zhangyan612@gmail.com>
2017-12-16 11:34:14 -0600

    add kanban board to project

---
+++ ProjectManager/Controllers/BoardController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectManager.Controllers
+{
+    public class BoardController : Controller
+    {
+        // GET: Board
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Kanban()
+        {
+            return View();
+        }
+
+        // GET: Board/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Board/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Board/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Board/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Board/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Board/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Board/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
