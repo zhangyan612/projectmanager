6d9ef6a424d1055ae8d688af4d54f57491dedb69
Yan Zhang <zhangyan612@gmail.com>
2018-06-28 08:44:44 -0500

    Update project to use angular

---
+++ ProjectManager/Scripts/app/projectlist.js
@@ -0,0 +1,47 @@
+﻿var app = angular.module('ProjectList', []);
+
+app.controller('ListController', function ($scope, DataService) {
+    // Default data (can be loaded from a database)
+    $scope.records = [
+        { state: 'CA', price: 22, tax: 5, include: false },
+        { state: 'MA', price: 32, tax: 8, include: false }
+    ];
+
+    $scope.editItem = function (item) {
+        console.log(item)
+        angular.element(document.getElementById("itemDetails")).scope().item = item;
+    };
+
+});
+
+
+app.controller('ItemController', function ($scope, DataService) {
+    $scope.saveItem = function (item) {
+        //db_list.push(item);
+        //$rootScope.item = null;
+    };
+});
+
+
+
+app.service('DataService', function () {
+    var myList = [
+        { state: 'CA', price: 22, tax: 5, include: false },
+        { state: 'MA', price: 32, tax: 8, include: false }
+    ];
+
+
+    var addList = function (newObj) {
+        myList.push(newObj);
+    }
+
+    var getList = function () {
+        return myList;
+    }
+
+    return {
+        addList: addList,
+        getList: getList
+    };
+
+});
