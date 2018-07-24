8ddf9e99528945f256fba14d388dc496c9651778
Yan Zhang <yan.zhang@insureon.com>
2018-07-23 23:10:08 -0500

    Change to using statement in service

--- ProjectManager/DAL/Services/ProjectTaskService.cs
+++ ProjectManager/DAL/Services/ProjectTaskService.cs
@@ -11,65 +11,72 @@ namespace ProjectManager.Services
 {
     public class ProjectTaskService
     {
-        private static PMContext db = new PMContext();
-
         public static List<ProjectTaskList> GetProjectTasks(Guid projectId)
         {
-            var tasks = db.Tasks.Where(x => x.ProjectId == projectId).Include(d => d.AssignedUsers).ToList();
-            List<ProjectTaskList> projectList = new List<ProjectTaskList>();
-
-            foreach (var t in tasks)
+            using (var db = new PMContext())
             {
-                ProjectTaskList projectTask = new ProjectTaskList();
-                ObjectMapper.Convert(t, projectTask);
+                var tasks = db.Tasks.Where(x => x.ProjectId == projectId).Include(d => d.AssignedUsers).ToList();
+                List<ProjectTaskList> projectList = new List<ProjectTaskList>();
 
-                projectTask.Status = t.Board.Name;
-                projectTask.AssignedUserList = t.AssignedUsers.ToList();
+                foreach (var t in tasks)
+                {
+                    ProjectTaskList projectTask = new ProjectTaskList();
+                    ObjectMapper.Convert(t, projectTask);
 
-                projectList.Add(projectTask);
+                    projectTask.Status = t.Board.Name;
+                    projectTask.AssignedUserList = t.AssignedUsers.ToList();
+
+                    projectList.Add(projectTask);
+                }
+                return projectList;
             }
-
-            return projectList;
         }
 
         public static TaskDescription GetTaskDescription(int id)
         {
-            var task = db.TaskDescription.Where(x => x.Id == id).First();
-            return task;
+            using (var db = new PMContext())
+            {
+                var task = db.TaskDescription.Where(x => x.Id == id).First();
+                return task;
+            }
         }
 
         public static int SaveTaskDescription(int id, string desc, string user)
         {
-            var task = db.Tasks.Where(x => x.Id == id).First();
+            using (var db = new PMContext())
+            {
+                var task = db.Tasks.Where(x => x.Id == id).First();
 
-            if(task.DescriptionId != null)
-            {
-                var existingDesc = db.TaskDescription.Where(x => x.Id == task.DescriptionId).First();
-                existingDesc.LastModifiedBy = user;
-                existingDesc.Description = desc;
-                existingDesc.LastModifiedDate = DateTime.Now;
-                db.Entry(existingDesc).State = EntityState.Modified;
-                db.SaveChanges();
-                return existingDesc.Id;
-            }
-            else
-            {
-                TaskDescription description = new TaskDescription()
+                if (task.DescriptionId != null)
                 {
-                    Description = desc,
-                    LastModifiedBy = user,
-                    LastModifiedDate = DateTime.Now
-                };
+                    var existingDesc = db.TaskDescription.Where(x => x.Id == task.DescriptionId).First();
+                    existingDesc.LastModifiedBy = user;
+                    existingDesc.Description = desc;
+                    existingDesc.LastModifiedDate = DateTime.Now;
+                    db.Entry(existingDesc).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return existingDesc.Id;
+                }
+                else
+                {
+                    TaskDescription description = new TaskDescription()
+                    {
+                        Description = desc,
+                        LastModifiedBy = user,
+                        LastModifiedDate = DateTime.Now
+                    };
 
-                db.TaskDescription.Add(description);
-                db.SaveChanges();
+                    db.TaskDescription.Add(description);
+                    db.SaveChanges();
 
-                int newId = description.Id;
-                task.DescriptionId = newId;
-                db.Entry(task).State = EntityState.Modified;
-                db.SaveChanges();
-                return newId;
+                    int newId = description.Id;
+                    task.DescriptionId = newId;
+                    db.Entry(task).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return newId;
+                }
             }
         }
+
     }
 }
